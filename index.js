6d4d50bd539a6c887fbe05e03b44747d98492a5b
sargsyansevak <thesevaksargsyan@gmail.com>
2023-08-20 10:31:25 -0500

    cirs

--- index.js
+++ index.js
@@ -25,12 +25,7 @@ mongoose
   .catch(() => console.log("DB error"));
 
 const app = express();
-app.use(cors(
-  {
-    // origin: "https://ntl-dusky.vercel.app/",
-    optionsSuccessStatus: 200,
-  }
-));
+app.use(cors());
 
 app.use(express.json());
 
